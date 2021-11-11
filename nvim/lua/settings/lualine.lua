f04954b3d4945878ecdd054edff5c0eae1b511d6
Velociburner <joshbp@brandeis.edu>
2021-11-11 18:24:43 -0500

    new syntax for custom separators, remove file format from statusline

--- nvim/lua/settings/lualine.lua
+++ nvim/lua/settings/lualine.lua
@@ -2,8 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'onedark',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
+    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''},
     disabled_filetypes = {}
   },
   sections = {
@@ -12,7 +12,6 @@ require('lualine').setup {
     lualine_c = {'filename'},
     lualine_x = {
       'encoding',
-      {'fileformat', icon = ''},
       {'filetype', icon = ''}
     },
     lualine_y = {'progress'},
