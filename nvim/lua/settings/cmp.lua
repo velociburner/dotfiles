b6a92bd4d629dbecf4b9ecc8e5bc72ba21d84e2b
Velociburner <joshbp@brandeis.edu>
2021-11-16 00:24:29 -0500

    switch from pyright to pylsp

--- nvim/lua/settings/cmp.lua
+++ nvim/lua/settings/cmp.lua
@@ -30,7 +30,7 @@ cmp.setup({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['pyright'].setup {
+require('lspconfig')['pylsp'].setup {
   capabilities = capabilities
 }
 
