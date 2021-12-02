8be00ad9e58ab248552dbbee0cc3e5f9d2d935ee
Velociburner <joshbp@brandeis.edu>
2021-12-01 21:50:07 -0500

    make project_files function global

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -4,7 +4,7 @@ tb = require('telescope.builtin')
 map('n', '<leader>fr', '<cmd>lua tb.resume()<cr>', opts)
 
 -- Find files using Telescope command-line sugar.
-map('n', '<leader>ff', '<cmd>lua require("settings.telescope").project_files()<cr>', opts)
+map('n', '<leader>ff', '<cmd>lua project_files()<cr>', opts)
 map('n', '<leader>fg', '<cmd>lua tb.live_grep()<cr>', opts)
 map('n', '<leader>fb', '<cmd>lua tb.buffers()<cr>', opts)
 map('n', '<leader>fh', '<cmd>lua tb.help_tags()<cr>', opts)
@@ -19,14 +19,10 @@ map('n', '<leader>ft', '<cmd>lua tb.treesitter()<cr>', opts)
 -- LSP diagnostics
 map('n', '<leader>fl', '<cmd>lua tb.lsp_document_diagnostics()<cr>', opts)
 
-local M = {}
-
-M.project_files = function()
+project_files = function()
   local opts = {}
   local ok = pcall(tb.git_files, opts)
   if not ok then
     tb.find_files(opts)
   end
 end
-
-return M
