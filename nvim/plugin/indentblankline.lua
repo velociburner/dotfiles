3f4acc096d94e651458d54b45d580bbe7c263131
Velociburner <joshbp@brandeis.edu>
2022-02-07 13:38:56 -0500

    remove highlight groups

--- nvim/plugin/indentblankline.lua
+++ nvim/plugin/indentblankline.lua
@@ -1,21 +1,6 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 require('indent_blankline').setup {
   buftype_exclude = {'help', 'terminal'},
   filetype_exclude = {'lspinfo', 'markdown', 'packer', 'text'},
-  char_highlight_list = {
-    'IndentBlanklineIndent1',
-    'IndentBlanklineIndent2',
-    'IndentBlanklineIndent3',
-    'IndentBlanklineIndent4',
-    'IndentBlanklineIndent5',
-    'IndentBlanklineIndent6',
-  },
   show_current_context = true,
   show_current_context_start = true,
 }
