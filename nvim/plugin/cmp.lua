1ff547b5562be7c969f3a6a0b9fe290cd007129d
Velociburner <joshbp@brandeis.edu>
2022-05-19 16:45:04 -0400

    explicitly define mappings to select next/prev autocomplete item

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -7,6 +7,8 @@ cmp.setup({
     end,
   },
   mapping = {
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
     ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
