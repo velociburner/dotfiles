ff7968b17ddd64d8aae73f9dcb7d99eee6cb2c0d
Velociburner <joshbp@brandeis.edu>
2022-06-06 14:17:14 -0400

    update lsp format setting

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -36,7 +36,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<localleader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<localleader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<localleader>f', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
   buf_set_keymap('x', '<localleader>f', '<Esc><cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
 
   vim.cmd [[
