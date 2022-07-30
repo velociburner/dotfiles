4a7c45fbb6fc13fd96ad17f0f24cf63951411c64
Velociburner <joshbp@brandeis.edu>
2022-07-30 18:50:15 -0400

    remove indent blankline trailing blankline indent

--- nvim/plugin/indentblankline.lua
+++ nvim/plugin/indentblankline.lua
@@ -3,4 +3,5 @@ require('indent_blankline').setup {
   filetype_exclude = {'lspinfo', 'markdown', 'packer', 'text'},
   show_current_context = true,
   show_current_context_start = true,
+  show_trailing_blankline_indent = false,
 }
