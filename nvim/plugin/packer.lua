972a9bbbac91e2cd64c86b05d5c1b166689e9b44
Velociburner <joshbp@brandeis.edu>
2022-08-12 12:53:03 -0400

    clean up requires

--- nvim/plugin/packer.lua
+++ nvim/plugin/packer.lua
@@ -1,43 +1,32 @@
+local function src_cfg(file)
+  return string.format("require('configs.%s')", file)
+end
+
 return require('packer').startup(function(use)
   ---Packer---
   use 'wbthomason/packer.nvim'
 
   ---Color scheme---
-  use { 'navarasu/onedark.nvim',
-    config = function() require('configs.onedark') end
-  }
+  use { 'navarasu/onedark.nvim', config = src_cfg('onedark') }
 
   ---Making my life easier---
   use 'tpope/vim-surround'
   use 'tpope/vim-repeat'
-  use { 'numToStr/Comment.nvim',
-    config = function() require('configs.comment') end
-  }
-  use { 'windwp/nvim-autopairs',
-    config = function() require('configs.autopairs') end
-  }
-  use { 'ggandor/lightspeed.nvim',
-    config = function() require('configs.lightspeed') end
-  }
+  use { 'numToStr/Comment.nvim', config = src_cfg('comment') }
+  use { 'windwp/nvim-autopairs', config = src_cfg('autopairs') }
+  use { 'ggandor/lightspeed.nvim', config = src_cfg('lightspeed') }
   use 'vim-scripts/ReplaceWithRegister'
-  use { 'preservim/nerdtree',
-    cmd = {'NERDTree', 'NERDTreeToggle'}
-  }
+  use { 'preservim/nerdtree', cmd = {'NERDTree', 'NERDTreeToggle'} }
 
   ---LaTeX---
-  use { 'lervag/vimtex',
-    ft = {'tex', 'bibtex'}
-  }
+  use { 'lervag/vimtex', ft = {'tex', 'bibtex'} }
 
   ---LSP---
-  use { 'neovim/nvim-lspconfig',
-    config = function() require('configs.lspconfig') end
-  }
+  use { 'neovim/nvim-lspconfig', config = src_cfg('lspconfig') }
 
   ---Autocomplete---
   use {
-    'hrsh7th/nvim-cmp',
-    config = function() require('configs.cmp') end,
+    'hrsh7th/nvim-cmp', config = src_cfg('cmp'),
     requires = {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
@@ -53,41 +42,29 @@ return require('packer').startup(function(use)
 
   ---Fuzzy finder---
   use 'nvim-lua/plenary.nvim'
-  use { 'nvim-telescope/telescope.nvim',
-    config = function() require('configs.telescope') end
-  }
+  use { 'nvim-telescope/telescope.nvim', config = src_cfg('telescope') }
 
   ---Git---
-  use { 'tanvirtin/vgit.nvim',
-    config = function() require('configs.vgit') end
-  }
+  use { 'tanvirtin/vgit.nvim', config = src_cfg('vgit') }
 
   ---REPL---
   use 'jpalardy/vim-slime'
 
   ---Statusline---
-  use { 'nvim-lualine/lualine.nvim',
-    config = function() require('configs.lualine') end
-  }
-  use { 'akinsho/bufferline.nvim',
-    config = function() require('configs.bufferline') end,
+  use { 'nvim-lualine/lualine.nvim', config = src_cfg('lualine') }
+  use { 'akinsho/bufferline.nvim', config = src_cfg('bufferline'),
     requires = 'kyazdani42/nvim-web-devicons'
   }
 
   ---Text Objects---
   use 'michaeljsmith/vim-indent-object'
   use 'wellle/targets.vim'
-  use { 'lukas-reineke/indent-blankline.nvim',
-    config = function() require('configs.indentblankline') end
-  }
+  use { 'lukas-reineke/indent-blankline.nvim', config = src_cfg('indentblankline') }
 
   ---Treesitter---
-  use { 'nvim-treesitter/nvim-treesitter',
-    config = function() require('configs.treesitter') end,
+  use { 'nvim-treesitter/nvim-treesitter', config = src_cfg('treesitter'),
     run = ':TSUpdate'
   }
   use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use { 'nvim-treesitter/playground',
-    cmd = 'TSPlaygroundToggle'
-  }
+  use { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' }
 end)
