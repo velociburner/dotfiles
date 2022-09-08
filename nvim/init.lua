e1e6d0f19d516f7eb25a740d7c1fa5b44838ca1a
Velociburner <joshbp@brandeis.edu>
2022-09-08 16:18:31 -0400

    vimtex conceal settings and mathbf snippet

--- nvim/init.lua
+++ nvim/init.lua
@@ -78,4 +78,18 @@ vim.g.slime_default_config = {socket_name = 'default', target_pane = ':.2'}
 -----------Vimtex----------- {{{
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+vim.g.vimtex_syntax_conceal = {
+  accents = 0,
+  ligatures = 0,
+  cites = 1,
+  fancy = 1,
+  greek = 0,
+  math_bounds = 0,
+  math_delimiters = 0,
+  math_fracs = 0,
+  math_super_sub = 0,
+  math_symbols = 0,
+  sections = 0,
+  styles = 0,
+}
 --}}}
