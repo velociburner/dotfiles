c8752d2713a9849709aba1ff7136ee793595815b
Velociburner <joshbp@brandeis.edu>
2023-06-08 11:53:06 -0400

    force cursor shapes to work in tmux and wezterm

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,7 @@ if status is-interactive
     set -gx EDITOR "nvim"
 
     # vi mode
+    set fish_vi_force_cursor 1
     set fish_cursor_default block
     set fish_cursor_insert line
     set fish_cursor_replace_one underscore
