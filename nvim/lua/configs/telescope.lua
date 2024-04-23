2f875512317672ce97c8ce0086a0092cf0f67640
--global <joshbp@brandeis.edu>
2024-04-22 22:56:42 -0400

    add telescope git keymaps

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -40,7 +40,7 @@ map('n', '<leader>fr', '<cmd>Telescope resume<cr>', opts)
 
 -- Find files using Telescope command-line sugar.
 map('n', '<leader>ff', '<cmd>lua require("configs.telescope").project_files()<cr>', opts)
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>fgr', '<cmd>Telescope live_grep<cr>', opts)
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
 
@@ -54,6 +54,10 @@ map('n', '<leader>ft', '<cmd>Telescope treesitter<cr>', opts)
 -- LSP
 map('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>', opts)
 map('n', '<leader>fs', '<cmd>Telescope lsp_document_symbols<cr>', opts)
+
+-- Git
+map("n", "<leader>fgs", '<cmd>Telescope git_status<cr>', opts)
+map("n", "<leader>fgb", '<cmd>Telescope git_branches<cr>', opts)
 
 -- File browser
 map("n", "<leader>fl", '<cmd>Telescope file_browser<cr>', opts)
