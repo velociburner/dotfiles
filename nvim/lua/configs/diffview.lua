a1f7c0badcaf8ef0f1efd7b5fa9d39567cbbc472
Velociburner <joshbp@brandeis.edu>
2024-04-24 17:00:02 -0400

    modify diffview keymaps

--- nvim/lua/configs/diffview.lua
+++ nvim/lua/configs/diffview.lua
@@ -4,5 +4,5 @@ local opts = { silent = true }
 map('n', '<leader>do', '<cmd>DiffviewOpen<cr>', opts)
 map('n', '<leader>dc', '<cmd>DiffviewClose<cr>', opts)
 map('n', '<leader>dt', '<cmd>DiffviewToggleFiles<cr>', opts)
-map({'n', 'v'}, '<leader>dh', ':DiffviewFileHistory %<cr>', opts)
-map('n', '<leader>dH', '<cmd>DiffviewFileHistory<cr>', opts)
+map({'n', 'v'}, '<leader>gl', ':DiffviewFileHistory %<cr>', opts)
+map('n', '<leader>gL', '<cmd>DiffviewFileHistory<cr>', opts)
